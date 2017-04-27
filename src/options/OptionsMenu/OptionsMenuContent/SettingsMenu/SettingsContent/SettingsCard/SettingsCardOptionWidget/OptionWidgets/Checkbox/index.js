cf9c874c3475cac3ffed989bd44a6d30698d37fe
eejdoowad <eejdoowad@gmail.com>
2017-04-27 17:00:55 -0400

    fixed bounding box on switch options widgets

--- src/options/OptionsMenu/OptionsMenuContent/SettingsMenu/SettingsContent/SettingsCard/SettingsCardOptionWidget/OptionWidgets/Checkbox/index.js
+++ src/options/OptionsMenu/OptionsMenuContent/SettingsMenu/SettingsContent/SettingsCard/SettingsCardOptionWidget/OptionWidgets/Checkbox/index.js
@@ -5,14 +5,18 @@ export default class Checkbox extends Component {
     return (
       <li className='mdc-list-item' style='justify-content: space-between'>
         <label>{label}</label>
-        <div class='mdc-checkbox' style='margin-right: -11px'>
-          <input type='checkbox' id='basic-checkbox' class='mdc-checkbox__native-control'
-            checked={value} onChange={(e) => onChange(e.target.checked)} />
-          <div class='mdc-checkbox__background'>
-            <svg version='1.1' class='mdc-checkbox__checkmark' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-              <path class='mdc-checkbox__checkmark__path' fill='none' stroke='white' d='M1.73,12.91 8.1,19.28 22.79,4.59' />
+        <div className='mdc-checkbox' style='margin-right: -11px'>
+          <input
+            type='checkbox'
+            className='mdc-checkbox__native-control'
+            checked={value}
+            onChange={(e) => onChange(e.target.checked)}
+          />
+          <div className='mdc-checkbox__background'>
+            <svg version='1.1' className='mdc-checkbox__checkmark' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+              <path className='mdc-checkbox__checkmark__path' fill='none' stroke='white' d='M1.73,12.91 8.1,19.28 22.79,4.59' />
             </svg>
-            <div class='mdc-checkbox__mixedmark' />
+            <div className='mdc-checkbox__mixedmark' />
           </div>
         </div>
       </li>
--- src/options/OptionsMenu/OptionsMenuContent/SettingsMenu/SettingsContent/SettingsCard/SettingsCardOptionWidget/OptionWidgets/Switch/index.js
+++ src/options/OptionsMenu/OptionsMenuContent/SettingsMenu/SettingsContent/SettingsCard/SettingsCardOptionWidget/OptionWidgets/Switch/index.js
@@ -1,15 +1,20 @@
 import { Component, h } from 'preact';
+import './style.css';
 
 export default class Checkbox extends Component {
   render ({ label, key, value, onChange }) {
     return (
       <li className='mdc-list-item' style='justify-content: space-between'>
         <label>{label}</label>
-        <div class='mdc-switch'>
-          <input type='checkbox' id='basic-switch' class='mdc-switch__native-control'
-            checked={value} onChange={(e) => onChange(e.target.checked)} />
-          <div class='mdc-switch__background'>
-            <div class='mdc-switch__knob' />
+        <div className='mdc-switch'>
+          <input
+            type='checkbox'
+            className='mdc-switch__native-control switch-widget-input-el'
+            checked={value}
+            onChange={(e) => onChange(e.target.checked)}
+          />
+          <div className='mdc-switch__background'>
+            <div className='mdc-switch__knob' />
           </div>
         </div>
       </li>
