63a8be3b08c4aa0f8044c7a4d88389f20d3b51a6
eejdoowad <eejdoowad@gmail.com>
2017-04-27 16:39:21 -0400

    fixed major bug that caused Saka Key to not load into preloaded pages. Preloaded pages are spawned when the user enters a URL into the URL bar. Attempting to execute the Saka client into these pages causes an error. Now, check for tab.index === -1 to make sure a page isn't preloaded, and listen for tabs.onReplaced to respawn the saka client bootloading process

--- src/background_page/modes.js
+++ src/background_page/modes.js
@@ -20,6 +20,10 @@ export async function modeMessage ({ mode, action, arg }, src) {
 
 export let cachedClientSettings;
 
+/**
+ * When a client requests settings, messages back the current client settings.
+ * Future: domain lookups to determine settings to message back
+ */
 export function clientSettings (arg, src) {
   msg(src, 'clientSettings', cachedClientSettings);
 }
@@ -58,23 +62,47 @@ async function generateClientSettings () {
   }
 }
 
-
 export function loadClient (_, src) {
-  const { frameId, tabId } = meta(src);
-  if (SAKA_DEBUG) {
-    console.log(`Loading client: frame: ${frameId}, tab" ${tabId}`);
-  }
-  try {
+  const { sender: { frameId, tab: { index: tabIndex, id: tabId } } } = meta(src);
+  // tabIndex === -1 indicates the tab is preloaded but not a full tab
+  // attempting to execute into it is an error
+  // http://stackoverflow.com/questions/43665470/cannot-call-chrome-tabs-executescript-into-preloaded-tab-is-this-a-bug-in-chr
+  if (tabIndex === -1) {
+    if (SAKA_DEBUG) {
+      console.log(`NOT loading client into preloaded tab: frame: ${frameId}, tab: ${tabId}`);
+    }
+  } else {
+    if (SAKA_DEBUG) {
+      console.log(`Loading client: frame: ${frameId}, tab: ${tabId}`);
+    }
     browser.tabs.executeScript(tabId, {
       file: '/content_script.js',
       frameId,
       runAt: 'document_start',
       matchAboutBlank: true
     });
-  } catch (e) {
-    if (SAKA_DEBUG) {
-      console.error(e);
-    }
   }
-
 }
+
+/**
+ * Requests for the full Saka Client by the client loaders of preloaded tabs are
+ * denied because attempting to execute within them would cause errors.
+ * http://stackoverflow.com/questions/43665470/cannot-call-chrome-tabs-executescript-into-preloaded-tab-is-this-a-bug-in-chr
+ * Instead, the background page listens for the tabs.onReplaced event and executes
+ * content_script_loader.js into all frames, which then launches another request for
+ * the full Saka Client
+ * @param {number} addedTabId
+ * @param {number} removedTabId
+ */
+function reloadClientLoaders (addedTabId, removedTabId) {
+  if (SAKA_DEBUG) {
+    console.log(`Tab id changed from ${removedTabId} to ${addedTabId}. Reloading content_script_loader.js`);
+  }
+  browser.tabs.executeScript(addedTabId, {
+    file: '/content_script_loader.js',
+    allFrames: true,
+    runAt: 'document_start',
+    matchAboutBlank: true
+  });
+}
+chrome.tabs.onReplaced.addListener(reloadClientLoaders);
