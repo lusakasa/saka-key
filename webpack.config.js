babdb27969ed15575395fdbe8192213e0ca7bdd3
YFdyh000 <yfdyh000@gmail.com>
2017-11-05 04:58:15 +0800

    Cleanup eslint warning for webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
-const webpack = require('webpack');
-const MinifyPlugin = require('babel-minify-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const GenerateJsonPlugin = require('generate-json-webpack-plugin');
-const merge = require('webpack-merge');
+const webpack = require('webpack')
+const MinifyPlugin = require('babel-minify-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const GenerateJsonPlugin = require('generate-json-webpack-plugin')
+const merge = require('webpack-merge')
+const path = require('path')
 
 // process.traceDeprecation = true;
 
 // markdown convert to html
-const marked = require('marked');
-const renderer = new marked.Renderer();
+const marked = require('marked')
+const renderer = new marked.Renderer()
 
 module.exports = function (env) {
-  console.log(env);
-  const [mode, platform, benchmark, firefoxBeta ] = env.split(':');
-  let version = require('./manifest/common.json').version;
-  if (firefoxBeta) version += 'beta';
+  console.log(env)
+  const [mode, platform, benchmark, firefoxBeta] = env.split(':')
+  let version = require('./manifest/common.json').version
+  if (firefoxBeta) version += 'beta'
 
   const config = {
     entry: {
-      'background_page': './src/background_page/index.js',
-      'content_script': './src/content_script/index.js',
-      'content_script_loader': './src/content_script/loader.js',
+      background_page: './src/background_page/index.js',
+      content_script: './src/content_script/index.js',
+      content_script_loader: './src/content_script/loader.js',
       // 'help': './src/help/index.js',
-      'extensions': './src/pages/extensions/index.js',
-      'info': './src/pages/info/index.js',
-      'options': './src/pages/options/index.js'
+      extensions: './src/pages/extensions/index.js',
+      info: './src/pages/info/index.js',
+      options: './src/pages/options/index.js'
       // 'popup': './src/popup/index.js',
     },
     output: {
-      path: __dirname + '/dist',
+      path: path.join(__dirname, '/dist'),
       filename: '[name].js',
       sourceMapFilename: '[name].js.map' // always generate source maps
     },
@@ -66,10 +67,7 @@ module.exports = function (env) {
       ]
     },
     resolve: {
-      modules: [
-        './src',
-        './node_modules'
-      ]
+      modules: ['./src', './node_modules']
     },
     plugins: [
       new webpack.optimize.ModuleConcatenationPlugin(),
@@ -88,44 +86,51 @@ module.exports = function (env) {
           to: 'config_[folder].json'
         }
       ]),
-      new GenerateJsonPlugin('manifest.json', merge(
-        require('./manifest/common.json'),
-        require(`./manifest/${platform}.json`),
-        { version }
-      ), null, 2)
+      new GenerateJsonPlugin(
+        'manifest.json',
+        merge(
+          require('./manifest/common.json'),
+          require(`./manifest/${platform}.json`),
+          { version }
+        ),
+        null,
+        2
+      )
     ]
-  };
+  }
 
   // extension id must be specified in calls to chrome.runtime.connect
   // otherwise clients won't be able to reconnect to background page
   // and background commands will stop working
-  const EXTENSION_ID = JSON.stringify(platform === 'chrome'
-    ? 'hhhpdkekipnbloiiiiaokibebpdpakdp'
-    : 'a0dd2b80-9ba1-224b-b5fe-3ae14f12d85d');
+  const EXTENSION_ID = JSON.stringify(
+    platform === 'chrome'
+      ? 'hhhpdkekipnbloiiiiaokibebpdpakdp'
+      : 'a0dd2b80-9ba1-224b-b5fe-3ae14f12d85d'
+  )
 
   if (mode === 'prod') {
     config.plugins = config.plugins.concat([
       new MinifyPlugin(),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production'),
-        'SAKA_DEBUG': JSON.stringify(false),
-        'SAKA_VERSION': JSON.stringify(version),
-        'SAKA_BENCHMARK': JSON.stringify(true),
-        'SAKA_PLATFORM': JSON.stringify(platform),
+        SAKA_DEBUG: JSON.stringify(false),
+        SAKA_VERSION: JSON.stringify(version),
+        SAKA_BENCHMARK: JSON.stringify(true),
+        SAKA_PLATFORM: JSON.stringify(platform),
         EXTENSION_ID
       })
-    ]);
+    ])
   } else {
     config.plugins = config.plugins.concat([
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('development'),
-        'SAKA_DEBUG': JSON.stringify(true),
-        'SAKA_VERSION': JSON.stringify(version + ' dev'),
-        'SAKA_BENCHMARK': JSON.stringify(benchmark === 'benchmark'),
-        'SAKA_PLATFORM': JSON.stringify(platform),
+        SAKA_DEBUG: JSON.stringify(true),
+        SAKA_VERSION: JSON.stringify(version + ' dev'),
+        SAKA_BENCHMARK: JSON.stringify(benchmark === 'benchmark'),
+        SAKA_PLATFORM: JSON.stringify(platform),
         EXTENSION_ID
       })
-    ]);
+    ])
   }
-  return config;
-};
+  return config
+}
