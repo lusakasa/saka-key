45c01d556aad82a24acf91784bba48606c5e1af2
Neil Macintyre <neil.macintyre@neilmacintyre.com>
2018-07-26 16:44:31 -0400

    Add blacklist feature

--- src/options/Blacklist/index.js
+++ src/options/Blacklist/index.js
@@ -2,7 +2,7 @@ import { getAttributes } from 'lib/util'
 
 export default (options, config) => {
   const backgroundOptions = getAttributes(options, ['blacklist'])
-  const clientOptions = {}
+  const clientOptions = getAttributes(options, ['blacklist'])
   const errors = {}
   return { backgroundOptions, clientOptions, errors }
 }
