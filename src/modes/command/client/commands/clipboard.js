a19561fc571abbc531d5ff39ea28611acc77db56
azzamsa <azzam@azzamsa.com>
2019-01-23 14:26:26 +0700

    Add ability to copy current url and title as org mode

--- src/modes/command/client/commands/clipboard.js
+++ src/modes/command/client/commands/clipboard.js
@@ -4,6 +4,18 @@ import { background } from 'lib/msg'
 export const copyURL = () => {
   copy(document.location.href)
 }
+
+export const copyToORG = () => {
+  function elideTitle () {
+    if (document.title.length > 80) {
+      return document.title.substr(0, 80 - 3).concat('...')
+    } else {
+      return document.title
+    }
+  }
+  copy('[[' + document.location.href + '][' + elideTitle() + ']]')
+}
+
 export const clipboardCurrentTab = background('clipboardCurrentTab')
 export const clipboardBackgroundTab = background('clipboardBackgroundTab')
 export const clipboardForegroundTab = background('clipboardForegroundTab')
