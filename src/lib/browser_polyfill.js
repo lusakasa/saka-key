c2761962bea9868317853b737a7e97a3b3048cb3
Brandon Kalinowski <brandon@brandonkalinowski.com>
2019-08-24 04:47:41 -0700

    feat: update webextension polyfill to use npm

--- src/lib/browser_polyfill.js
+++ src/lib/browser_polyfill.js
@@ -1,5 +1,3 @@
-// TODO: at some point remove this polyfill or if an npm package is released
-// use it, instead of a random github repo
 if (SAKA_PLATFORM === 'chrome') {
-  require('webextension-polyfill/dist/browser-polyfill')
+  require('webextension-polyfill')
 }
